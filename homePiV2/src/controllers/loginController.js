81eaf3b7bbec14acb7f9873dc6e93c42179d2aa6
Johannes <johannes001002@gmail.com>
2022-07-26 22:33:16 -0300

    Adicionando o pacote bcrypt e encriptando a senha.

--- homePiV2/src/controllers/loginController.js
+++ homePiV2/src/controllers/loginController.js
@@ -1,5 +1,5 @@
 const userRepository = require('../database/seeders/UserCreate');
-
+const bcrypt = require('bcrypt')
 
 module.exports = {
 
@@ -10,11 +10,28 @@ module.exports = {
 
     // POST LOGIN *VERIFICAÇÃO DE USUÁRIO DENTRO DO "BANCO DE DADOS"
     loginPost : (req, res) => {
-        const {email} = req.body;
-
+        const {email, password} = req.body;
         let dataUserToLogin = userRepository.findUserByField('email', email);
+
+        // verificando se há email
         if(dataUserToLogin){
-            res.send('Você foi encontrado no banco de dados')
+            // verificando se a senha encriptada coincide com a senha recebida do body
+            let isPasswordVerified = bcrypt.compareSync(password, dataUserToLogin.passwordEncrypted)
+
+            // Se a senha coincidir ele irá fazer isso
+            if(isPasswordVerified){
+                delete dataUserToLogin.senha;
+                res.send('Autenticado com sucesso')
+            }   
+
+            if(!isPasswordVerified){
+                res.send('Email ou senha incorreto.')
+            }
+        }
+
+
+        if(!dataUserToLogin){
+            res.send('Infelizmente você não tem conta malandro. Vai fazer uma conta pra já!!!')
         }
     },
 
@@ -25,8 +42,13 @@ module.exports = {
 
     // POST (registro no banco de dados)
     registrationPost: (req, res) => {
-        const dataUser = req.body;
-        let user = userRepository.create(dataUser);
+        // Pegando os dados do body 
+        const {nomeCompleto, cpf, dataNascimento, email, tel, password} = req.body;
+        // Criptografando a senha
+        const passwordEncrypted = bcrypt.hashSync(password, 12);
+
+        // Criando utilizando o seeder o usuário e enviando para o users.json.
+        let user = userRepository.create({nomeCompleto, cpf, dataNascimento, email, tel, passwordEncrypted});
         if(user){
             return res.render('registConfirm', {title: 'Cadastrado com sucesso'});
         }
