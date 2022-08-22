b312e4e03a75f886799334cdfd2236ceaf52b955
Johannes <johannes001002@gmail.com>
2022-08-22 19:51:43 -0300

    session

--- homePiV2/src/controllers/cartController.js
+++ homePiV2/src/controllers/cartController.js
@@ -1,23 +1,24 @@
-
 module.exports = {
-    cartPage : (req, res, next) => {
-        const KEY_STORAGE = "@homepiv2:cart";
-        let cart = req.cookies[KEY_STORAGE];
-        if (!cart){
-            cart= '[]';
-        }
-        cart = JSON.parse(cart);
-        const cartFormated = cart.map(item =>({
-            ...item,
-            subTotal:item.price * item.amount
-        }))
-        const total = cartFormated.reduce((acc, current) =>acc+Number(current.subTotal), 0);
-        res.render('shoppingCart', { 
-            title: 'Seu carrinho',
-            cart:cartFormated,
-            total
-        });
+  cartPage: (req, res, next) => {
+    const KEY_STORAGE = "@homepiv2:cart";
+    let cart = req.cookies[KEY_STORAGE];
+    if (!cart) {
+      cart = "[]";
     }
+    cart = JSON.parse(cart);
+    const cartFormated = cart.map((item) => ({
+      ...item,
+      subTotal: item.price * item.amount,
+    }));
+    const total = cartFormated.reduce(
+      (acc, current) => acc + Number(current.subTotal),
+      0
+    );
+    res.render("shoppingCart", {
+      title: "Seu carrinho",
+      cart: cartFormated,
+      total,
+      userEmail: req.cookies.email,
+    });
+  },
 };
-
-
--- homePiV2/src/controllers/indexController.js
+++ homePiV2/src/controllers/indexController.js
@@ -1,35 +1,47 @@
-const { validationResult } = require('express-validator');
-const db = require('../database/models');
+const { validationResult } = require("express-validator");
+const db = require("../database/models");
 
 module.exports = {
-    index : async (req, res)  => {
-        const produtos = await db.Product.findAll();
+  index: async (req, res) => {
+    const produtos = await db.Product.findAll();
 
-        res.render('index', {
-            produtos, 
-            title: 'DH_Sports' });
-    },
-    privacy : (req, res)  => {
-        res.render('privacy', { title: 'Politica de Privacidade' });
-    },
-    termsUse : (req, res)  => {
-        res.render('termsUse', { title: 'Termos de Uso' });
-    },
-    exchangePolicy : (req, res)  => {
-        res.render('exchangePolicy', { title: 'Politica de troca' });
-    },
-    contact : (req, res)  => {
-        res.render('contact', { title: 'Entre em contato' });
-    },
-    contactConfirm : (req, res)  => {
-        const errors = validationResult(req, res);
-        if (errors.isEmpty()) {
-            res.render('contactConfirm', { title: 'Mensagem enviada!' });
+    res.render("index", {
+      produtos,
+      title: "DH_Sports",
+      userEmail: req.cookies.email,
+    });
+  },
 
-        }else {
-            res.render('contact', { 
-                errors: errors.mapped(),
-                title : 'OPS!'});
-        }
+  logout: (req, res) => {
+    const { logout } = req.body;
+
+    if (logout) {
+      res.clearCookie("email");
+      res.redirect("/");
     }
+  },
+
+  privacy: (req, res) => {
+    res.render("privacy", { title: "Politica de Privacidade" });
+  },
+  termsUse: (req, res) => {
+    res.render("termsUse", { title: "Termos de Uso" });
+  },
+  exchangePolicy: (req, res) => {
+    res.render("exchangePolicy", { title: "Politica de troca" });
+  },
+  contact: (req, res) => {
+    res.render("contact", { title: "Entre em contato" });
+  },
+  contactConfirm: (req, res) => {
+    const errors = validationResult(req, res);
+    if (errors.isEmpty()) {
+      res.render("contactConfirm", { title: "Mensagem enviada!" });
+    } else {
+      res.render("contact", {
+        errors: errors.mapped(),
+        title: "OPS!",
+      });
+    }
+  },
 };
